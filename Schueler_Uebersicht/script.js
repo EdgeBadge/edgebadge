63ba598072245a2d33f72a11fec140da99841bda
christophfuhrer <christoph.fuhrer@live.at>
2014-12-02 13:24:18 +0100

    Schüler Übersicht Update

--- Schueler_Uebersicht/script.js
+++ Schueler_Uebersicht/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-    
+    $('.showbtn').click( function(){
+    	var subject = $(this).attr('data-subject') + "";
+    	$('.fachbox').css('display', 'none');
+    	$('.subjects').html('<button class="btn_back1"></button>' + subject);
+    	$('.btn_back1').css('display','block');
+    	$('.btn_back1').css('float','left');
+    	$('.btn_back1').css('bottom','0px');
+    	$('.fachansicht').css('display','block');
+    });
    
 });
