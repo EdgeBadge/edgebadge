68e835090dff68d77fd259f9c3e5834ecc596453
christophfuhrer <christoph.fuhrer@live.at>
2014-12-09 10:02:16 +0100

    Schüler Website Update

--- Schueler_Uebersicht/script.js
+++ Schueler_Uebersicht/script.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
-    $('.showbtn').click( function(){
+    $('.showbtn').click(function(){
     	var subject = $(this).attr('data-subject') + "";
+    	var teacher = $(this).attr('data-teacher') + "";
+        $('.subjects').css('margin-left','10%');
     	$('.fachbox').css('display', 'none');
-    	$('.subjects').html('<button class="btn_back1"></button>' + subject);
+    	$('.subjects').html('<button class="btn_back1"></button>' + subject + "<br /> <span class='subjectteacher'></span>");
     	$('.btn_back1').css('display','block');
     	$('.btn_back1').css('float','left');
     	$('.btn_back1').css('bottom','0px');
     	$('.fachansicht').css('display','block');
+    	$('.subjectteacher').text(teacher);
+    	$('.subjectteacher').css('display','block');
+        
+        $('.btn_back1').click(function(){
+            $('.subjects').html('<div class="subjects"> MEINE FÄCHER <div class="sort_btns"> <button class="sort1">A</button><button class="sort2">B</button></div></div');
+            $('.fachansicht').css('display','none');
+            $('.fachbox').css('display', 'block');
+            $('.subjectteacher').css('display','none');
+            $('.subjects').css('margin-left','5%');
+        });
     });
-   
+    
 });
