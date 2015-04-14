2594681063dd56c96999f3e204c6434d52d62b85
Raphael Niederhauser <raphael@niederhauser.co.at>
2015-04-14 11:23:59 +0200

    nvm

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -7,7 +7,24 @@
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
   	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Montserrat" type="text/css">
   	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans" type="text/css">
+  	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+
+  <!-- sidebar -->
+  <div id="eb-sidebar">
+    <div class="eb-logo">Edge<span class="bold">Badge</span></div>
+    <div class="profil_pic"><img src="images/IMG_1120.jpg" class="img-circle" width="120" height="120"></div>
+    <div class="user_name">Alexander Rauscher</div>
+    <ul class="sidebar-nav list-unstyled">
+      <li><a href="#"><span class="glyphicon glyphicon-home"></span>DASHBOARD</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-file"></span>BADGE ERSTELLEN</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-send"></span>BADGE AUSSTELLEN</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-cog"></span>PROFIL BEARBEITEN</a></li>
+    </ul>
+  </div>
+   <!-- ende: sidebar -->
+
+
 </body>
 </html>
